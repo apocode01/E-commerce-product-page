561475863686c24a6ff8bd8feaa47d40472fc5a7
apocode01 <apopanou@gmail.com>
2025-09-19 21:03:08 +0300

    began process of implementing the cart functionality

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const mainImgBtn = document.querySelector(`.mainImgBtn`);
-
 const mainImages = document.querySelectorAll(`.main-image`);
-
 const previewImgButtons = document.querySelectorAll(`.previewImgBtn`);
-
 const previewImages = document.querySelectorAll(`.preview-image`);
 
 previewImgButtons.forEach((previewImageBtn, index) => {
@@ -21,4 +17,41 @@ previewImgButtons.forEach((previewImageBtn, index) => {
 
         mainImages[index].classList.remove(`hidden`);
     })
+})
+
+const decreaseBtn = document.querySelector(`.decreaseBtn`);
+const increaseBtn = document.querySelector(`.increaseBtn`);
+const selectedProductAmountSpan = document.querySelector(`.selected-product-amount`);
+const addToCartBtn = document.querySelector(`.add-to-cart-btn`);
+let selectedProductAmount = 0;
+let productAmount = 0;
+
+decreaseBtn.addEventListener(`click`, () => {
+    if (selectedProductAmount >= 1) {
+        selectedProductAmount--;
+    }
+    selectedProductAmountSpan.textContent = selectedProductAmount;
+});
+
+increaseBtn.addEventListener(`click`, () => {
+    selectedProductAmount++;
+    selectedProductAmountSpan.textContent = selectedProductAmount;
+});
+
+addToCartBtn.addEventListener(`click`, () => {
+    productAmount += selectedProductAmount;
+    console.log(productAmount);
+})
+
+const cartBtn = document.querySelector(`.cartBtn`);
+const cartMenu = document.querySelector(`.products-in-cart`);
+
+cartBtn.addEventListener(`click`, () => {
+    cartMenu.classList.toggle(`shown`);
+})
+
+const deleteItemBtn = document.querySelector(`.deleteItemBtn`);
+
+deleteItemBtn.addEventListener(`click`, () => {
+    console.log("delete");
 })
